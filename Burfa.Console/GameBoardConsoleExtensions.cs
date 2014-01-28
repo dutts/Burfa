64adf1cc5c5dd7180f76199398c367db80113f9b
Richard <richard@dutton.me.uk>
2014-01-28 19:01:51 +0000

    Added row and column numbering

--- Burfa.Console/GameBoardConsoleExtensions.cs
+++ Burfa.Console/GameBoardConsoleExtensions.cs
@@ -14,15 +14,29 @@ namespace Burfa.Console
             System.Console.Clear();
             for (int i = 0; i < gb.BoardEdgeLength; i++)
             {
-                System.Console.Write("|---");
+                if (i == 0) System.Console.Write("  ");
+                if (i == gb.BoardEdgeLength - 1) System.Console.WriteLine("  " + i + " ");
+                else
+                {
+                    System.Console.Write("  " + i + " ");
+                }
+            }
+            for (int i = 0; i < gb.BoardEdgeLength; i++)
+            {
+                if (i == 0) System.Console.Write("  |---");
+                else
+                {
+                    System.Console.Write("|---");
+                }
                 if (i == gb.BoardEdgeLength - 1) System.Console.WriteLine("|");
             }
             for (int i = 0; i < gb.BoardEdgeLength; i++)
             {
+                System.Console.Write(i + " ");
                 for (int j = 0; j < gb.BoardEdgeLength; j++)
                 {
-                    var gameSquare = gb.GetGameBoardSquare(j, i);
                     System.Console.Write("| ");
+                    var gameSquare = gb.GetGameBoardSquare(j, i);
                     System.Console.Write(gameSquare.State == null ? " " : gameSquare.State == Player.Black ? "B" : "W");
                     System.Console.Write(" ");
                     
@@ -30,7 +44,14 @@ namespace Burfa.Console
                 }
                 for (int j = 0; j < gb.BoardEdgeLength; j++)
                 {
-                    System.Console.Write("|---");
+                    if (j == 0)
+                    {
+                        System.Console.Write("  |---");
+                    }
+                    else
+                    {
+                        System.Console.Write("|---");
+                    }
                     if (j == gb.BoardEdgeLength - 1) System.Console.WriteLine("|");
                 }
             }
